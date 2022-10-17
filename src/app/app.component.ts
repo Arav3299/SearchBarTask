92cd87182e368d3b49f232d1fe3b3e0540f0904f
Arav3299 <aravindharaj1999@gmail.com>
2022-10-17 18:53:54 +0530

    for host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,18 +17,18 @@ export class AppComponent implements AfterViewInit{
  @ViewChild('nameef')nameERef!:ElementRef; 
   user:any;
   paths = [
-    { id: 1, name: 'sachincricket', path: 'http://localhost:4200/assets/image/sachin1.jfif' },
-    { id: 2, name: 'dhonicricket' , path: 'http://localhost:4200/assets/image/dhoni.png'},
-    { id:13, name: 'rohitcricket' , path: 'http://localhost:4200/assets/image/rohit.jpg'},
-    { id:14, name: 'cr7football' , path: 'http://localhost:4200/assets/image/cr7.jfif' },
-    { id: 5, name: 'messifootball' , path: 'http://localhost:4200/assets/image/messi.jfif'},
-    { id: 6, name: 'neymarfootball' , path: 'http://localhost:4200/assets/image/ney.jfif'},
-    { id: 7, name: 'kohlicricket' , path: 'http://localhost:4200/assets/image/kohli.jfif'},
-    { id: 8, name: 'pvsindhubadminton' , path: 'http://localhost:4200/assets/image/pv.jfif'},
-    { id: 9, name: 'ViswanathanAnanthchess' , path: 'http://localhost:4200/assets/image/viswanathanchess.jpg'},
-    { id: 10, name: 'magnuscarlsanchess' , path: 'http://localhost:4200/assets/image/magnuschess.jfif'},
-    { id: 11, name: 'sainanehwalbadminton' , path: 'http://localhost:4200/assets/image/sain.jpg'}
-    ,{ id: 12, name: 'undertakerwwe' , path: 'http://localhost:4200/assets/image/under.jpg'}
+    { id: 1, name: 'sachincricket', path: '/assets/image/sachin1.jfif' },
+    { id: 2, name: 'dhonicricket' , path: '/assets/image/dhoni.png'},
+    { id:13, name: 'rohitcricket' , path: '/assets/image/rohit.jpg'},
+    { id:14, name: 'cr7football' , path: '/assets/image/cr7.jfif' },
+    { id: 5, name: 'messifootball' , path: '/assets/image/messi.jfif'},
+    { id: 6, name: 'neymarfootball' , path: '/assets/image/ney.jfif'},
+    { id: 7, name: 'kohlicricket' , path: '/assets/image/kohli.jfif'},
+    { id: 8, name: 'pvsindhubadminton' , path: '/assets/image/pv.jfif'},
+    { id: 9, name: 'ViswanathanAnanthchess' , path: '/assets/image/viswanathanchess.jpg'},
+    { id: 10, name: 'magnuscarlsanchess' , path: '/assets/image/magnuschess.jfif'},
+    { id: 11, name: 'sainanehwalbadminton' , path: '/assets/image/sain.jpg'}
+    ,{ id: 12, name: 'undertakerwwe' , path: '/assets/image/under.jpg'}
   ];
   name: any;
 test='uihsihsiushsiu<p></p>';
